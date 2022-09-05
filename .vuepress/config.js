76743f17f56acf18ab878a5e24fcce7be834af55
hexiaopi <12226042+Hexiaopi@users.noreply.github.com>
2022-09-05 18:00:30 +0800

    fix: 去除base参数，避免静态资源引入导致的路径问题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,6 @@ module.exports = {
   title: "何小进童鞋",
   description: '个人博客',
   dest: 'public',
-  base: '/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
